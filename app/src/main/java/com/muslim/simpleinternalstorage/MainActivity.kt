5d8984c9a71c18711a128033370bc8f6deea88ea
Murad <engno_95@mail.ru>
2023-06-04 13:59:34 +0300

    Internal Storage

--- app/src/main/java/com/muslim/simpleinternalstorage/MainActivity.kt
+++ app/src/main/java/com/muslim/simpleinternalstorage/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.muslim.simpleinternalstorage
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,11 +13,17 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.muslim.simpleinternalstorage.ui.theme.SimpleInternalStorageTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +33,9 @@ class MainActivity : ComponentActivity() {
                 val textState = remember {
                     mutableStateOf("")
                 }
+                val context = LocalContext.current
+                val scope = rememberCoroutineScope()
+
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally,
@@ -39,7 +49,9 @@ class MainActivity : ComponentActivity() {
                     Spacer(modifier = Modifier.height(20.dp))
 
                     Button(onClick = {
-
+                        scope.launch{
+                            textState.value = readFile(context)
+                        }
                     }) {
                         Text(text = "Read")
                     }
@@ -47,13 +59,43 @@ class MainActivity : ComponentActivity() {
                     Spacer(modifier = Modifier.height(20.dp))
 
                     Button(onClick = {
-
+                        scope.launch {
+                            save(context)
+                        }
                     }) {
                         Text(text = "Save")
                     }
                 }
             }
         }
+    }
+}
+
+private suspend fun save(context: Context) {
+    val text = "У лука моря дуб зеленый;\n" +
+            "Златая цепь на дубе том:\n" +
+            "И днем и ночью кот ученый\n" +
+            "Все ходит по цепи кругом\n" +
+            "Идет направо - песень заводит\n" +
+            "Налево - сказку говорит."
+
+    withContext(Dispatchers.IO) {
+        context.openFileOutput("test.txt", Context.MODE_PRIVATE).use {
+            it.write(text.toByteArray())
+        }
+    }
+}
+
+private suspend fun readFile(context: Context) = withContext(Dispatchers.IO){
+    try {
+        context.openFileInput("test.txt").bufferedReader().useLines {lines ->
+            lines.fold("") {acc, s ->
+                "$acc\n$s"
+            }
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+        ""
     }
 }
 
